3b1e2a74ef37a8bd159d6ba4113e3d40f75101cd
Howie Cheng <howiechen@sjtu.edu.cn>
2019-04-03 17:07:42 +0800

    非托管方式DLL

---
+++ dcam/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace dcam
+{
+    class Program
+    {
+        [DllImport("../CAMAPI.dll", EntryPoint = "Add", CallingConvention = CallingConvention.StdCall)]
+        public static extern int Add(int x, int y);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(10, 12));
+            Console.ReadKey();
+        }
+    }
+}
