d0b7ecd6cdb319cacc7dc308bb3c75012fdb83b2
Howie Cheng <howiechen@sjtu.edu.cn>
2019-04-04 16:28:42 +0800

    struct pointer

--- dcam/Program.cs
+++ dcam/Program.cs
@@ -6,14 +6,44 @@ using System.Text;
 
 namespace dcam
 {
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct Test
+    {
+        public int a;
+        public int b;
+        //[MarshalAs(UnmanagedType.SysInt, SizeConst = 32)]
+        //public String User;
+    }
+
     class Program
     {
         [DllImport("../CAMAPI.dll", EntryPoint = "Add", CallingConvention = CallingConvention.StdCall)]
         public static extern int Add(int x, int y);
+        [DllImport("../CAMAPI.dll", EntryPoint = "ceateTest", CallingConvention = CallingConvention.StdCall)]
+        //public static extern void  ceateTest(int x, int y, IntPtr test);// IntPtr
+        public static extern void createTest(int x, int y, ref Test test);// ref
+        [DllImport("../CAMAPI.dll", EntryPoint = "callTestByPoint", CallingConvention = CallingConvention.StdCall)]
+        public static extern int callTest(ref Test testPtr);
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Add(10, 12));
+            // Console.WriteLine(Add(10, 12)); // test transfer by value
+
+            //var res = new Test();
+            //IntPtr pP1 = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Test)));
+            //Marshal.WriteByte(pP1, 0);
+            //Marshal.StructureToPtr(res, pP1, true);
+            //ceateTest(10, 120, pP1);
+            //res = (Test)Marshal.PtrToStructure(pP1, typeof(Test));
+
+            var res = new Test();
+            createTest(10, 20, ref res);
+
+            //var res = new Test();
+            //res.a = 10;
+            //res.b = 20;
+            Console.WriteLine(callTest(ref res));
             Console.ReadKey();
         }
     }
